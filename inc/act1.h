41f16733250c55347626b175fe756bd762320e54
Pavansk172 <53371524+Pavansk172@users.noreply.github.com>
2021-04-30 18:48:14 +0530

    Update act1.h

--- inc/act1.h
+++ inc/act1.h
@@ -10,8 +10,8 @@
  */
 
 
-#ifndef _ACT1_H
-#define _ACT1_H
+#ifndef _ACTIVITY1_H
+#define _ACTIVITY1_H
 
 #define AVR_ATmega328
 
@@ -21,25 +21,25 @@
  * @brief A macro to check if the button is pressed or not.
  * 
  */
-#define SENSOR_ON !(PIND&(1<<PD0))
+#define SENSOR_ON !(PIND&(1<<PD4))
 
 /**
  * @brief A macro to check if the heater is pressed or not.
  * 
  */
-#define HEATER_ON !(PIND&(1<<PD4))
+#define HEATER_ON !(PIND&(1<<PD0))
 
 /**
  * @brief A macro to turn on the LED 
  * 
  */
-#define SET_LED PORTD|=(1<<PD6)
+#define SET_LED PORTD|=(1<<PD2)
 
 /**
  * @brief A macro to turn off the LED
  * 
  */
-#define CLEAR_LED PORTD&=~(1<<PD6)
+#define CLEAR_LED PORTD&=~(1<<PD2)
 
 void Buttons_LED_Init();
 
